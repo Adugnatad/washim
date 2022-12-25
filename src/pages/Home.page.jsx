6f737393087a70df74623325af01b3d1e3243b3c
HawiTad <adugtad@gmail.com>
2022-12-25 16:09:40 +0300

    d

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -9,7 +9,7 @@ const Home = () => {
                 <p className='w-[80%] text-[20px] md:text-[25px]'>WASHIMS is an Innovative smart approach to water and utility management  focused largely on automation, real-time view of hydraulic data and better data management.</p>
             </section>
 
-            <section className='flex flex-col md:flex-row items-center justify-between bg-gradient-to-r  from-[#0082CE] to-[#004172] p-10  md:px-[70px] md:my-10  md:mx-20'>
+            <section className='flex flex-col md:flex-row items-center justify-between bg-gradient-to-r  from-[#0082CE] to-[#004172] p-10 md:px-[70px] md:my-10  md:mx-20'>
                 <div className='flex flex-row flex-wrap justify-center md:w-[354px] mr-5'>
                     <div className=' flex flex-col items-center justify-center w-fit mb-3 mr-3'>
                         <span className='bg-white rounded-[10px] w-[106px] h-[105px] flex items-center justify-center'>  <img src='/assets/communitiesIcon.png' alt='' /> </span>
@@ -47,17 +47,17 @@ const Home = () => {
                 </div>
             </section>
 
-            <section className='flex flex-col cu:flex-row items-center justify-center bg-[#F6F6F6]'>
+            <section className='flex flex-col cu:flex-row items-center justify-center bg-[#F6F6F6] p-0'>
                 <img src="/assets/womanFetchingWater.png" alt="" style={{ width: "500px" }} />
-                <div className='cu:w-[50%] p-[80px] md:p-[100px] text-[18px]'>
-                    <h1 className='text-[28px] font-bold '>
+                <div className='cu:w-[50%] p-[80px] md:p-[100px] '>
+                    <h1 className='text-[23px] md:text-[28px] font-bold '>
                         Updated National estimates for WASH
                         in households: the Country is off track for SDG WASH targets
                     </h1>
-                    <p className='mt-7 mb-5'>
+                    <p className='mt-7 mb-2 md:text-[18px]'>
                         Updated country, regional and Communities estimates are now available for the period 2000 to 2020. Achieving universal coverage by 2030 will require a quadrupling of current rates of progress in safely managed drinking water, safely managed sanitation, and basic hygiene services.
                     </p>
-                    <p>
+                    <p className='md:text-[18px]'>
                         WASHIMS will provide reliable and affordable water and sanitation data which compliant with regulatory standards and ensure that operations and assets are sustainable.
                     </p>
                 </div>
